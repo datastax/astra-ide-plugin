4346089e023e2a116ae480898a021786d36727d8
Garrett Conaty <garrett@conaty.net>
2021-05-14 14:54:59 -0700

    update to intellij plugin 1.0-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "astra-intellij-plugin"
+
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
