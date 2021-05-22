da4ed7461174837f2581dc7556f1a65ef2eb4b80
Garrett Conaty <garrett@conaty.dev>
2021-05-21 17:29:41 -0700

    tidy up create db dialog. separate out API parameters completely from UI elements

--- src/main/kotlin/com/datastax/astra/jetbrains/services/database/CreateDatabaseDialog.kt
+++ src/main/kotlin/com/datastax/astra/jetbrains/services/database/CreateDatabaseDialog.kt
@@ -1,21 +1,18 @@
 package com.datastax.astra.jetbrains.services.database
 
 import com.datastax.astra.devops_v2.models.AvailableRegionCombination
-import com.datastax.astra.devops_v2.models.Costs
 import com.datastax.astra.devops_v2.models.DatabaseInfoCreate
-import com.datastax.astra.devops_v2.models.RegionCombination
 import com.datastax.astra.jetbrains.AstraClient
 import com.datastax.astra.jetbrains.MessagesBundle.message
 import com.datastax.astra.jetbrains.explorer.DatabaseParentNode
 import com.datastax.astra.jetbrains.explorer.ExplorerToolWindow
 import com.datastax.astra.jetbrains.explorer.refreshTree
 import com.datastax.astra.jetbrains.utils.ApplicationThreadPoolScope
-import com.intellij.codeInsight.daemon.impl.createActionLabel
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
-import com.intellij.ui.RightAlignedLabelUI
+import com.intellij.ui.CollectionComboBoxModel
+import com.intellij.ui.components.JBRadioButton
 import com.intellij.ui.layout.*
 import com.intellij.util.ui.tree.TreeUtil
 import kotlinx.coroutines.CoroutineScope
@@ -23,10 +20,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.annotations.NotNull
 import java.awt.Component
-import javax.swing.ButtonGroup
-import javax.swing.JComboBox
 import javax.swing.JComponent
-import javax.swing.JRadioButton
 import javax.swing.tree.DefaultMutableTreeNode
 
 class CreateDatabaseDialog(
@@ -35,45 +29,23 @@ class CreateDatabaseDialog(
 ) : DialogWrapper(project, parent, false, IdeModalityType.PROJECT),
     CoroutineScope by ApplicationThreadPoolScope("Database") {
     var allRegions: List<AvailableRegionCombination> = emptyList()
-    init{
+
+    init {
         CreateDatabaseGetRegions(project)
         runBlocking { allRegions = CreateDatabaseGetRegions.getRegions() }
     }
-    //TODO:Add all regions for all providers to same group but different panels. Avoid setting wrong region from another page
 
+    //Property values for create database params
     var name: String = ""
     var keyspace: String = ""
-
     var cloudProvider = DatabaseInfoCreate.CloudProvider.AWS
-
-    //TODO: Show/change the tier when the selection is made/changed
     var tier = DatabaseInfoCreate.Tier.SERVERLESS
-    var region = "us-west-2"
+    val regionForProvider: MutableMap<String, AvailableRegionCombination> = mutableMapOf()
 
-    val awsButton = JRadioButton("AWS")
-    val gcpButton = JRadioButton("GCP")
-    val azureButton = JRadioButton("Azure")
-    var providerGroup = ButtonGroup()
-
-    //TODO: Generate these ComboBoxes based on the providers listed in allRegions
-    var awsRegionsComboBox = ComboBox<DatabaseComboBoxItem>()
-    var gcpRegionsComboBox = ComboBox<DatabaseComboBoxItem>()
-    var azureRegionsComboBox = ComboBox<DatabaseComboBoxItem>()
-
+    //UI variables
+    lateinit var providerButtons: Map<String, JBRadioButton>
 
     val view = panel {
-        providerGroup.add(awsButton)
-        providerGroup.add(gcpButton)
-        providerGroup.add(azureButton)
-        allRegions.forEach {
-            if(it.tier=="serverless")
-            when(it.cloudProvider) {
-                "AWS" -> awsRegionsComboBox.addItem(DatabaseComboBoxItem(it))
-                "GCP" -> gcpRegionsComboBox.addItem(DatabaseComboBoxItem(it))
-                "AZURE" -> azureRegionsComboBox.addItem(DatabaseComboBoxItem(it))
-            }
-        }
-
         row("Database Name:") {
             textField(::name).withValidationOnApply {
                 if (it.text.trim().isEmpty()) ValidationInfo(
@@ -92,23 +64,33 @@ class CreateDatabaseDialog(
         }
         row("Cloud Provider:") {
             cell {
-                awsButton()
-                gcpButton()
-                azureButton()
+                buttonGroup(::cloudProvider) {
+                    providerButtons = listOf("AWS", "GCP", "AZURE").associate {
+                        it to radioButton(it, DatabaseInfoCreate.CloudProvider.AWS).component
+                    }
+                }
             }
         }
         row("Tier:") {
             label(tier.value)
         }
-        row {
-            label("Region:")
+        row("Region:") {
             cell{
-                awsRegionsComboBox().visibleIf(awsButton.selected).withLeftGap().component.setMinimumAndPreferredWidth(160)
-                gcpRegionsComboBox().visibleIf(gcpButton.selected).withLeftGap().component.setMinimumAndPreferredWidth(160)
-                azureRegionsComboBox().visibleIf(azureButton.selected).withLeftGap().component.setMinimumAndPreferredWidth(160)
+                allRegions
+                    .filter { it.tier == "serverless" }
+                    .filter { enumValues<DatabaseInfoCreate.CloudProvider>().any { enum -> enum.value == it.cloudProvider } }
+                    .groupBy { it.cloudProvider }
+                    .forEach { regionsByProvider ->
+                        regionForProvider[regionsByProvider.key] = regionsByProvider.value.first()
+                        comboBox(CollectionComboBoxModel(regionsByProvider.value),
+                            getter = { regionForProvider[regionsByProvider.key] },
+                            setter = { it?.apply { regionForProvider[regionsByProvider.key] = it } },
+                            renderer = listCellRenderer { value, _, _ -> text = value.region })
+                            .visibleIf(providerButtons[regionsByProvider.key]!!.selected)
+                            //.withLeftGap()//.component.setMinimumAndPreferredWidth(160)
+                    }
             }
         }
-
     }
 
     init {
@@ -128,29 +110,11 @@ class CreateDatabaseDialog(
         if (!okAction.isEnabled) {
             return
         }
-
-        //This is where we designate where to make a DB based on the selection
-        when {
-            awsButton.selected() -> {
-                cloudProvider=DatabaseInfoCreate.CloudProvider.AWS
-                region = awsRegionsComboBox.selectedItem.toString()
-            }
-            gcpButton.selected() -> {
-                cloudProvider=DatabaseInfoCreate.CloudProvider.GCP
-                region = gcpRegionsComboBox.selectedItem.toString()
-            }
-            azureButton.selected() -> {
-                //Can't do this
-                //TODO: Fail gracefully
-            }
-        }
-
         view.apply()
         close(OK_EXIT_CODE)
         launch {
-
             //TODO: Wouldn't it be nice if this structure had a mapper directly to the View values?
-            val databaseInfoCreate = DatabaseInfoCreate(name, keyspace, cloudProvider, tier, 1, region)
+            val databaseInfoCreate = DatabaseInfoCreate(name, keyspace, cloudProvider, tier, 1, regionForProvider[cloudProvider.value]?.region.orEmpty())
             val response = AstraClient.dbOperationsApi().createDatabase(databaseInfoCreate)
             if (response.isSuccessful) {
                 val databaseParent =
