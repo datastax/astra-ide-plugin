35f0931a467465af24fd05ffc4de34712f51eb1a
Matt Forrest <mforrest10@gmail.com>
2021-07-07 14:26:47 -0500

    Added additional functions and dialogs

--- src/main/kotlin/com/datastax/astra/jetbrains/credentials/UserRegisterDialog.kt
+++ src/main/kotlin/com/datastax/astra/jetbrains/credentials/UserRegisterDialog.kt
@@ -5,12 +5,14 @@ import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.jcef.JBCefApp
 import com.intellij.ui.jcef.JBCefBrowser
+import org.cef.browser.CefBrowser
 import org.intellij.markdown.*
 import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.Dimension
 import javax.swing.JComponent
 import javax.swing.JPanel
+
 
 class UserRegisterDialog(
     private val project: Project,
@@ -20,7 +22,12 @@ class UserRegisterDialog(
 
     val view = JPanel(BorderLayout())
 
+
+
+
+
     val myBrowser = JBCefBrowser("astra.datastax.com/register")
+    
 
     init {
 
@@ -28,11 +35,11 @@ class UserRegisterDialog(
             // Fallback to an alternative browser-less solution
         }
         view.setPreferredSize(Dimension(450, 600))
-        view.add(myBrowser.component)
+        view.add(myDevToolsBrowser.component)
         title = "Register"
         setOKButtonText("Done Registering")
         init()
-        this.setModal(false)
+
     }
 
     override fun createCenterPanel(): JComponent = view
@@ -45,7 +52,7 @@ class UserRegisterDialog(
 
     // If user clicks cancel dispose of the current browser session
     override fun doCancelAction() {
-        Disposer.dispose(myBrowser)
+        Disposer.dispose(myDevToolsBrowser)
         close(CANCEL_EXIT_CODE)
     }
 }
