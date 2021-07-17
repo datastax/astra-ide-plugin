411e0d9d6e9182e87d7cde133a27f480339f0685
Garrett Conaty <garrett@conaty.net>
2021-07-16 17:16:58 -0700

    Upgrade gradle and plugins
    - Bump gradle to 7.1.1
    - org.jetbrains.kotlin.jvm 1.5.21
    - org.jetbreains.intellij 1.1.3
    - org.jetbrains.changelog 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.changelog.date
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -8,11 +9,11 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("org.jetbrains.kotlin.jvm") version "1.5.21"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.0"
+    id("org.jetbrains.intellij") version "1.1.3"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "1.1.2"
+    id("org.jetbrains.changelog") version "1.2.0"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
     id("io.gitlab.arturbosch.detekt") version "1.17.1"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
@@ -95,14 +96,16 @@ intellij {
 // Configure gradle-changelog-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-    version = properties("pluginVersion")
-    groups = emptyList()
+    version.set(properties("pluginVersion"))
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
 }
 
+/*
 ktlint {
     enableExperimentalRules.set(true)
     disabledRules.set(setOf("experimental:package-name", "no-wildcard-imports"))
 }
+*/
 
 // Configure detekt plugin.
 // Read more: https://detekt.github.io/detekt/kotlindsl.html
@@ -117,18 +120,30 @@ detekt {
     }
 }
 
+// Configure UI tests plugin
+// Read more: https://github.com/JetBrains/intellij-ui-test-robot
 tasks {
-    // Set the compatibility versions to 1.8
-    withType<JavaCompile> {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+
+    buildSearchableOptions {
+        //Disabled due to failure to run with 2020.3.4
+        enabled = false
     }
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+    // Set the JVM compatibility versions
+    properties("javaVersion").let {
+        withType<JavaCompile> {
+            sourceCompatibility = it
+            targetCompatibility = it
+        }
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = it
+        }
+        withType<Detekt> {
+            jvmTarget = it
+        }
     }
 
-    withType<Detekt> {
-        jvmTarget = "1.8"
+    wrapper {
+        gradleVersion = properties("gradleVersion")
     }
 
     patchPluginXml {
@@ -155,6 +170,13 @@ tasks {
 
     runPluginVerifier {
         ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+    }
+
+    runIdeForUiTests {
+            systemProperty("robot-server.port", "8082")
+            systemProperty("ide.mac.message.dialogs.as.sheets", "false")
+            systemProperty("jb.privacy.policy.text", "<!--999.999-->")
+            systemProperty("jb.consents.confirmation.enabled", "false")
     }
 
     publishPlugin {
