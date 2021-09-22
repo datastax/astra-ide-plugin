4d2dbaec555ccbdc1b7873196f25496d6d7acbb9
Matt Forrest <mforrest10@gmail.com>
2021-09-22 12:49:19 -0500

    Add outline for doing test

--- src/test/kotlin/BatchDocWriterUnitTest.kt
+++
@@ -1,7 +0,0 @@
-class BatchDocWriterUnitTest {
-
-    //TODO:
-    // Make sure client sends proper requests for with a JSON field ID and without (Mock server)
-    // Make sure all functions of unit behave properly
-
-}
